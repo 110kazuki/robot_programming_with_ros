08b3c972995849497997db3e1fcc99ffe603150e
110kazuki <ito.k@dsc.mech.eng.osaka-u.ac.jp>
2022-04-23 04:15:07 +0900

    add arduino sample, motor_control_type_lap

--- arduino/motor_control/motor_control_type_lap/motor_control_type_lap.ino
+++ arduino/motor_control/motor_control_type_lap/motor_control_type_lap.ino
@@ -1,4 +1,4 @@
-/* [ DC motor control with ESCON ]
+/* [ DC motor control with Locked anti phase PWM type motor driver ]
  *  version : 1.0 (20220422)
  *  Auther  : Kazuki Ito
  */
@@ -9,19 +9,21 @@
 #include <std_msgs/Int16.h>
 
 /* ==== User setting ==== */
-//motor driver setting
-#define PWM_FREQUENCY  3000 //PWM pulse frequency
-#define PWM_RESOLUTION 10   //Duty ratio resolution, 10bit (0~1023)
+//motor driver property
+#define PWM_FREQUENCY  3000     //PWM pulse frequency
+#define PWM_RESOLUTION 10       //Duty ratio resolution, 10bit (0~1023)
+#define PWM_duty_limit_under 10 //[%] Lower limit of PWM duty ratio
+#define PWM_duty_limit_upper 90 //[%] Upper limit of PWM duty ratio
 
 //ESP32 pin number setting
-#define MOTOR_EN_PIN 18     //pin number connect with ESCON EN channel
-#define PWM_PIN      17     //pin number connect with ESCON PWM channel
-/* ====================== */
+#define PWM_PIN      17         //pin number connect with PWM input channel
+#define MOTOR_EN_PIN 18         //pin number connect with enable channel (option)
+/* ======================== */
 
-#define MOTOR_0        0    //PWM channel
-
-int motor_cmd = 0;         // motor command speed, specified as an integer value from -100 to 100.
+#define MOTOR_0 0           //PWM channel
+int motor_cmd = 0;          // motor command speed, specified as an integer value from -100 to 100.
 int pwm_cmd   = 0;         
+int PWM_duty_range = (pow( 2, PWM_RESOLUTION )-1) * (PWM_duty_limit_under + (100-PWM_duty_limit_upper)) * 0.005;
 
 //ros setting
 ros::NodeHandle nh;
@@ -50,12 +52,14 @@ void setup() {
   //desable ESCON (safety)
   digitalWrite( MOTOR_EN_PIN, LOW );
 
+  PWM_duty_limit_under = pow( 2, PWM_RESOLUTION ) * PWM_duty_limit_under * 0.01;
+  PWM_duty_limit_upper = pow( 2, PWM_RESOLUTION ) * PWM_duty_limit_upper * 0.01;
 }
 
 void loop() {
   if ( nh.connected() ){
     //convert motor command to duty ratio
-    pwm_cmd = constrain( motor_cmd * 4.092, 103, 920 );
+    pwm_cmd = constrain( motor_cmd * PWM_duty_range, PWM_duty_limit_under, PWM_duty_limit_upper );
 
     digitalWrite( MOTOR_EN_PIN, HIGH );
     ledcWrite( MOTOR_0, 511 + pwm_cmd );    
